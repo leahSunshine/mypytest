cdec3423bab483d70ba89314d96ce685b904d72d
leahSunshine <1049913728@qq.com>
2022-11-13 17:38:01 +0800

    mypytest

---
+++ testcase/mypytest.py
@@ -0,0 +1,13 @@
+
+
+def test_demo1():
+    assert 100 == 100
+    
+def test_demo2():
+    assert 100 == 99
+    
+def test_demo3():
+    assert 77 == 77
+    
+def test_demo4():
+    assert 88 == 99
