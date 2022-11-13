8e3a78762b5c55a70408c9c38690a1257197de37
leahSunshine <1049913728@qq.com>
2022-11-13 19:47:29 +0800

    mypytest

--- testcase/mypytest.py
+++ testcase/mypytest.py
@@ -1,5 +1,6 @@
 
 
+import re
 def test_demo1():
     assert 100 == 100
     
@@ -11,3 +12,10 @@ def test_demo3():
     
 def test_demo4():
     assert 88 == 99
+
+def test_re():
+    str=""""filter_checkbox" onChange="filterTable(this)" type="checkbox"/><span class="passed">2 passed</span>, <input checked="true" class="filter" data-test-result="skipped" """
+    regular = re.findall(r'span class="passed">(\d*?) passed<', str)
+    print(regular)
+    
+test_re()
